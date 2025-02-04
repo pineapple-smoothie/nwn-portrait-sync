f7318bba1943b208838aebc02ed2af9ac7325c06
pineapple-smoothie <>
2025-02-04 16:26:45 +0200

    Removes unused mailer

--- app/mailers/registration_mailer.rb
+++
@@ -1,8 +0,0 @@
-class RegistrationMailer < ApplicationMailer
-  layout "mailer"
-
-  def confirm(user)
-    @user = user
-    mail subject: "Confirm your email address", to: user.email_address
-  end
-end
