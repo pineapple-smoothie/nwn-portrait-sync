0ec1500a48cbbbbdb45f4312b9c627ed96cc56fa
pineapple-smoothie <>
2025-02-04 12:19:32 +0200

    Produce images without alpha channel

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -7,40 +7,50 @@ class Conversion < ApplicationRecord
       gravity: "north",
       extent: "256x512",
       background: "black",
+      flatten: true,
+      alpha: "off",
       format: :tga,
-      alpha: "remove"
+      define: "tga:alpha-type=0"
 
     attachable.variant :tga_large,
       resize_to_fill: [ 128, 200 ],
       gravity: "north",
       extent: "128x256",
       background: "black",
+      flatten: true,
+      alpha: "off",
       format: :tga,
-      alpha: "remove"
+      define: "tga:alpha-type=0"
 
     attachable.variant :tga_medium,
       resize_to_fill: [ 64, 100 ],
       gravity: "north",
       extent: "64x128",
       background: "black",
+      flatten: true,
+      alpha: "off",
       format: :tga,
-      alpha: "remove"
+      define: "tga:alpha-type=0"
 
     attachable.variant :tga_small,
       resize_to_fill: [ 32, 50 ],
       gravity: "north",
       extent: "32x64",
       background: "black",
+      flatten: true,
+      alpha: "off",
       format: :tga,
-      alpha: "remove"
+      define: "tga:alpha-type=0"
 
     attachable.variant :tga_tiny,
       resize_to_fill: [ 16, 25 ],
       gravity: "north",
       extent: "16x32",
       background: "black",
+      flatten: true,
+      alpha: "off",
       format: :tga,
-      alpha: "remove"
+      define: "tga:alpha-type=0"
   end
 
   validates :image, attached: true
